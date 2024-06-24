9a58301dac32fc1405a1d1f7416a83db30adccf0
Abenezer Umeta <abenezernachew50@gmail.com>
2024-06-24 15:28:16 +0300

    Add API documentation with drf-spectacular
    Integrated drf-spectacular to generate API documentation.
    
        - Added drf-spectacular settings to the project.
        - Created schema and documentation views.
    
    This provides a comprehensive and interactive API documentation interface.

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
+from drf_spectacular.utils import extend_schema
 
 
 class CategoryView(viewsets.ViewSet):
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     # internal app
     "ecommerce.product",
 ]
@@ -111,3 +112,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
